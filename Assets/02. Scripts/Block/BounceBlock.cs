03edcce6b78f152244c2b627f699f4be313c5158
NaDeNeunNa <75741403+NaDeNeunNa@users.noreply.github.com>
2020-12-11 00:15:44 +0900

    스프링 수정

--- Assets/02. Scripts/Block/BounceBlock.cs
+++ Assets/02. Scripts/Block/BounceBlock.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class BounceBlock : MonoBehaviour
 {
-    public float BounceSpeed = 7;
+    public bool YMove = false;
+    public float YPower = 7;
+    public bool XMove = false;
+    public float XPower = 7;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Debug.Log(collision.tag);
@@ -13,7 +17,15 @@ public class BounceBlock : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().velocity = (collision.transform.position - new Vector3(transform.position.x, transform.position.y, collision.transform.position.z)).normalized * BounceSpeed;
+            //collision.gameObject.GetComponent<Rigidbody2D>().velocity = (collision.transform.position - new Vector3(transform.position.x, transform.position.y, collision.transform.position.z)).normalized * BounceSpeed;
+            if (YMove)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(collision.gameObject.GetComponent<Rigidbody2D>().velocity.x, YPower, 0);
+            }
+            if (XMove)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(XPower, collision.gameObject.GetComponent<Rigidbody2D>().velocity.y, 0);
+            }
         }
     }
 }
