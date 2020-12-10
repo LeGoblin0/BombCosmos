4f29788a7eb63dc5fdd629b0cb6cb2840a4bd082
NaDeNeunNa <75741403+NaDeNeunNa@users.noreply.github.com>
2020-12-10 21:51:22 +0900

    프로

---
+++ Assets/02. Scripts/BGMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMove : MonoBehaviour
+{
+    public Transform Target;
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Target.position.x * speed, Target.position.y * speed, transform.position.z);
+    }
+}
